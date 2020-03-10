c3bee31c9cbec1cd66b853c9818dddea9b73e7c2
Austin Redmond <aredmond85@gmail.com>
2020-03-10 03:12:31 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,7 @@
 #use each_with_index
 def roll_call_dwarves(array)
   array.each_with_index do |item, index|
-    index += 1
-    puts "#{index}. #{item}"
+    puts "#{index + 1}. #{item}"
   end
 end
 
@@ -19,11 +18,7 @@ end
 #array of calls
 #method tells us where not anything is longer than 4 characters in the array
 def long_planeteer_calls(calls)
-  if calls.any? { |call| call.length > 4  }
-    return true
-  else
-    return false
-  end
+  calls.any? { |call| call.length > 4  }
 end
 
 #accept 1 array of strings
